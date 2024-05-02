2d6442ae952656be279840abd19fada10a17cb46
EtimGeorge <saencrystal@gmail.com>
2024-05-02 20:03:09 +0100

    commit 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript functionality (if needed)
+// Example: Toggle navigation menu for smaller screens
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
